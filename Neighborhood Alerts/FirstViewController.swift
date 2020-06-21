062ea2f3c6f042eda83c1104aaa4423f162051e1
Jeffrey Wang <j@mywikis.com>
2020-06-20 19:17:55 -0500

    Initial Commit

---
+++ Neighborhood Alerts/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Neighborhood Alerts
+//
+//  Created by Jeffrey Wang on 6/20/20.
+//  Copyright © 2020 MyWikis LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Neighborhood Alerts/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Neighborhood Alerts
+//
+//  Created by Jeffrey Wang on 6/20/20.
+//  Copyright © 2020 MyWikis LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
