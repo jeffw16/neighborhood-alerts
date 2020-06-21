650b550904f25708df4543aa123b280288dda38b
Amit <amit.joshiusa@gmail.com>
2020-06-21 11:11:33 -0500

    its basically right but it can't find firebase auth pls fix this lollll

---
+++ Neighborhood Alerts/DetailedAlertViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailedAlertViewController.swift
+//  Neighborhood Alerts
+//
+//  Created by Amit Joshi on 6/20/20.
+//  Copyright © 2020 MyWikis LLC. All rights reserved.
+//
+
+import UIKit
+
+class DetailedAlertViewController: UIViewController {
+    
+    var alertTitle: String?
+    var alertDescription: String?
+    var alertImage: UIImage?
+    var alertAuthor: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func viewWillAppear(_ animated: Bool) {
+        
+        titleLabel.text = alertTitle! ?? ""
+        
+        authorLabel.text = "Posted by " + alertAuthor! ?? ""
+        
+        descriptionLabel.text = alertDescription! ?? ""
+        
+        if alertImage != nil {
+            imageView.image = alertImage
+        }
+        
+        
+        
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Neighborhood Alerts/ListViewController.swift
+++ Neighborhood Alerts/ListViewController.swift
@@ -7,16 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class Alert {
+    var id: String // GUID of alert location
+    var displayName: String // what's displayed on the map
     
-    var alertsList: [String] = [] // change type later to Alert type
+    var description: String
+    var category: String
+    var image: UIImage?
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+    var author: String
+
+
+    
+    init(id: String, displayName: String, description: String, category: String, image: UIImage?, latitude: CLLocationDegrees, longitude: CLLocationDegrees, author: String) {
+        self.id = id
+        self.displayName = displayName
+        self.description = description
+        self.category = category
+        self.image = image ?? nil
+
+        self.latitude = latitude
+        self.longitude = longitude
+        self.author = author
+    }
+}
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var alertsList: [Alert] = []
+    
+    
+
+    let detailedAlertSegueIdentifier = "DetailedAlertSegueIdentifier"
     
     let alertCellIdentifier: String = "AlertCellIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        var alert1 = Alert(id: "ae34f1134", displayName: "Fire on Fake Street", description: "Theres a fire bro what more do u want", category: "Fire", image: nil, latitude: CLLocationDegrees(39.127112), longitude: CLLocationDegrees(-77.533134), author: "Jeffrey Wang")
         
+        alertsList.append(alert1)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,17 +60,38 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: alertCellIdentifier, for: indexPath)
         
         let rowNum = indexPath.row
         
-//        cell.textLabel?.text = alertsList[rowNum].SOME_ATTRIBUTE_CONTAINING_TEXT
+        cell.textLabel?.text = alertsList[rowNum].displayName
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         tableView.deselectRow(at: indexPath, animated: true)
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == detailedAlertSegueIdentifier {
+            let nextVC = segue.destination as! DetailedAlertViewController
+            
+            let alertIndex:Int = tableView.indexPathForSelectedRow?.row ?? 0
+            
+            nextVC.alertTitle = alertsList[alertIndex].displayName
+            
+            nextVC.alertDescription = alertsList[alertIndex].description
+            
+            nextVC.alertImage = alertsList[alertIndex].image
+            
+            nextVC.alertAuthor = alertsList[alertIndex].author
+            
+            
+        }
     }
 
 
