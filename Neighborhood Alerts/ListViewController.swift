690d0d974369c26381a71bea33010ecd8c21862b
Jeffrey Wang <j@mywikis.com>
2020-06-20 21:13:02 -0500

    Adding the four base tab views

--- Neighborhood Alerts/ListViewController.swift
+++ Neighborhood Alerts/ListViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var alertsList: [String] = [] // change type later to Alert type
+    
+    let alertCellIdentifier: String = "AlertCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alertsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: alertCellIdentifier, for: indexPath)
+        
+        let rowNum = indexPath.row
+        
+//        cell.textLabel?.text = alertsList[rowNum].SOME_ATTRIBUTE_CONTAINING_TEXT
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
 
 
 }
