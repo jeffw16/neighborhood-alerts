d2a710e1d7a8cd3bc199d98c82669a2549e01221
Jeffrey Wang <j@mywikis.com>
2020-07-09 00:29:23 -0500

    Adding create account empty blanks alert

--- Neighborhood Alerts/CreateAccountViewController.swift
+++ Neighborhood Alerts/CreateAccountViewController.swift
@@ -37,6 +37,24 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     
     // actual actions to create the account
     func createAccount() {
+        // check if anything is blank
+        if emailAddressField.text == ""
+            || passwordField.text == ""
+            || phoneNumberField.text == ""
+            || nameField.text == ""
+            || homeAddressField.text == "" {
+            let alertController = UIAlertController(
+                title: "Account creation failed",
+                message: "Please fill in all fields to create an account.",
+                preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        // actually create the account on Firebase
         Auth.auth().createUser(withEmail: emailAddressField.text!, password: passwordField.text!) { authResult, error in
             if error != nil || Auth.auth().currentUser == nil {
                 // error creating an account
@@ -54,9 +72,6 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
             } else {
                 // successful login, proceed to log in
                 let db = Firestore.firestore()
-                // generate UUID for the document ID
-//                let uuidIdentifier = UUID()
-//                let userUuidFinal = uuidIdentifier.uuidString.lowercased()
                 db.collection("users").document(self.emailAddressField.text!).setData([
                     "fullName": self.nameField.text!,
                     "homeAddress": self.homeAddressField.text!,
